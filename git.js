eebed65dd6cc45c4f0aa08ceab152bc134f9891b
krishna <krishnacodes17@gmail.com>
2025-04-28 08:52:02 +0530

    adding some functions

--- git.js
+++ git.js
@@ -1,1 +1,21 @@
-f
+const a =25;
+const bc = 30;
+const c = 35;
+
+function add(a, bc) {
+    return a + bc;
+}
+console.log(add(a, bc));
+
+ function subtract(a, b) {
+    return a - b;
+ }
+    console.log(subtract(b, c));
+
+const m = 5;
+const n = 10;
+
+function multiply(m, n) {
+    return m * n;
+}
+console.log(multiply(m, n));
